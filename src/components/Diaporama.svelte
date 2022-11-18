a638dc9354645660febf20f4d811c3b7290a5643
Sylvain <chambon.sylvain@gmail.com>
2022-11-18 23:05:53 +0100

    fix:diapo/ commentaires fonctions

--- src/components/Diaporama.svelte
+++ src/components/Diaporama.svelte
@@ -152,22 +152,32 @@
     }
 
     if (!isPause) {
-      showTransition().then(() => {
+      showTransition("transition", 1000).then(() => {
         timer(durationGlobal ?? durations[currentQuestion] ?? 10)
       })
     }
     currentDuration = durationGlobal ?? durations[currentQuestion] ?? 10
   }
 
-  async function showTransition() {
-    const dialog = document.getElementById("transition")
+  /**
+   * Afficher un écran (élément <dialog>) pendant un nombre de millisecondes
+   * @param {string} dialogId id de l'élément <dialog> à activer
+   * @param {number} nbOfMilliseconds durée de l'affichage en ms
+   */
+  async function showTransition(dialogId: string, nbOfMilliseconds: number) {
+    const dialog = document.getElementById(dialogId)
     if (dialog) {
       dialog.showModal()
-      await sleep(1000)
+      await sleep(nbOfMilliseconds)
       dialog.close()
     }
   }
 
+  /**
+   * Faire une pause pendant l'exécution d'un programme
+   * {@link https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep?page=1&tab=scoredesc#tab-top | Source}
+   * @param ms nb de millisecondes de la pause
+   */
   const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms))
 
   async function setSize() {
