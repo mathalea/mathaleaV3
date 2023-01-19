799ae8bba60983d4cddf576139d7127683cbbb68
Rémi Angot <remi.angot@gmail.com>
2023-01-19 06:56:35 +0100

    import from v2

--- src/modules/dictionnaireDesExercicesAleatoires.js
+++ src/modules/dictionnaireDesExercicesAleatoires.js
@@ -3571,7 +3571,7 @@ export default {
     "interactifType": "mathLive",
     "name": "3G44",
     "newEx": {
-      "isNew": true,
+      "isNew": false,
       "tag": "<span class=\"ui mini orange label transition visible\">NEW</span>"
     },
     "updateEx": {
@@ -5357,6 +5357,21 @@ export default {
     "name": "4G20-2",
     "newEx": {
       "isNew": false,
+      "tag": "<span class=\"ui mini orange label transition visible\">NEW</span>"
+    },
+    "updateEx": {
+      "isNewFeat": false,
+      "tag": "<span class=\"ui mini orange label transition visible\">MAJ</span>"
+    }
+  },
+  "4G20-3": {
+    "titre": "Calculer un carré",
+    "url": "/exercices/4e/4G20-3.js",
+    "amcReady": false,
+    "interactifReady": false,
+    "name": "4G20-3",
+    "newEx": {
+      "isNew": true,
       "tag": "<span class=\"ui mini orange label transition visible\">NEW</span>"
     },
     "updateEx": {
@@ -8631,7 +8646,7 @@ export default {
     "interactifReady": false,
     "name": "6C30-9",
     "newEx": {
-      "isNew": true,
+      "isNew": false,
       "tag": "<span class=\"ui mini orange label transition visible\">NEW</span>"
     },
     "updateEx": {
@@ -9040,7 +9055,7 @@ export default {
     "interactifReady": false,
     "name": "6G13-1",
     "newEx": {
-      "isNew": true,
+      "isNew": false,
       "tag": "<span class=\"ui mini orange label transition visible\">NEW</span>"
     },
     "updateEx": {
@@ -9175,7 +9190,7 @@ export default {
     "interactifReady": false,
     "name": "6G21-4",
     "newEx": {
-      "isNew": true,
+      "isNew": false,
       "tag": "<span class=\"ui mini orange label transition visible\">NEW</span>"
     },
     "updateEx": {
@@ -9190,7 +9205,7 @@ export default {
     "interactifReady": false,
     "name": "6G21-5",
     "newEx": {
-      "isNew": true,
+      "isNew": false,
       "tag": "<span class=\"ui mini orange label transition visible\">NEW</span>"
     },
     "updateEx": {
@@ -9491,7 +9506,8 @@ export default {
     "titre": "Trouver l'image d'une figure par une symétrie axiale dans un pavage",
     "url": "/exercices/6e/6G25-3.js",
     "amcReady": false,
-    "interactifReady": false,
+    "interactifReady": true,
+    "interactifType": "mathLive",
     "name": "6G25-3",
     "newEx": {
       "isNew": false,
