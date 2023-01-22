04fe214f020736a820319481781232cc57efddc2
Rémi Angot <remi.angot@gmail.com>
2023-01-22 13:08:49 +0100

    Compatibilité ES2015

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from "vite"
-import { svelte } from "@sveltejs/vite-plugin-svelte"
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/beta/",
+  base: '/beta/',
+  build: {
+    target: 'es2015'
+  },
   plugins: [
     svelte({
       compilerOptions: {
