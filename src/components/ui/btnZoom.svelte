f322e7c074ef1c29c18ab60ebfa00598600a27d4
Rémi Angot <remi.angot@gmail.com>
2023-03-07 18:33:11 +0100

    Ajout du Zoom dans les 2 premières vues eleve

---
+++ src/components/ui/btnZoom.svelte
@@ -0,0 +1,37 @@
+<script lang='ts'>
+  import { Mathalea } from "../../Mathalea"
+  import { exercicesParams, globalOptions } from "../store"
+
+  export let size : 'xs' | 'sm' | 'md' | 'lg' = 'sm' 
+
+  const urlParams = new URLSearchParams(window.location.search)
+  let zoom = parseInt(urlParams.get('z')) || 1
+  function zoomMinus() {
+    zoom -= 0.25
+    updateSize()
+  }
+
+  function zoomPlus() {
+    zoom += 0.25
+    updateSize()
+  }
+
+  function updateSize() {
+    globalOptions.update((params) => {
+      params.z = zoom.toString()
+      return params
+    })
+    Mathalea.updateUrl($exercicesParams)
+  }
+</script>
+
+<button type="button" on:click={zoomMinus} class="tooltip tooltip-left tooltip-neutral" data-tip="Réduire la taille du texte"
+  ><i
+    class="bx bx-{size}  px-2 bx-zoom-out hover:text-coopmaths-action-lightest text-coopmaths-action dark:text-coopmathsdark-action dark:hover:text-coopmathsdark-action-lightest"
+  /></button
+>
+<button type="button" on:click={zoomPlus} class="tooltip tooltip-left tooltip-neutral" data-tip="Augmenter la taille du texte"
+  ><i
+    class="bx bx-{size} px-2 bx-zoom-in hover:text-coopmaths-action-lightest text-coopmaths-action dark:text-coopmathsdark-action dark:hover:text-coopmathsdark-action-lightest"
+  /></button
+>
