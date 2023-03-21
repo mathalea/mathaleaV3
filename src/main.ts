03a5989f7b008df6b2d05f6ac38a1db49e0649f5
Jean-Claude Lhote <lhotejc@gmail.com>
2023-03-21 19:58:18 +0100

    Ceci est un test... pour voir si le wrapper de BigInt fonctionne sur IOS12

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import 'boxicons/css/boxicons.min.css'
 import './app.css'
 import App from './components/App.svelte'
-
+import * as BigInt from 'bigint-polyfill'
+window.BigInt = BigInt
 const app = new App({
   target: document.getElementById('app')
 })
