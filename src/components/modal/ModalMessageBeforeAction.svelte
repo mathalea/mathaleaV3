047b6622a82648847611336afc3ab089e6141781
sylcha <chambon.sylvain@gmail.com>
2023-03-26 19:42:28 +0200

    doc: pour ModalMessageBeforeAction

--- src/components/modal/ModalMessageBeforeAction.svelte
+++ src/components/modal/ModalMessageBeforeAction.svelte
@@ -3,8 +3,8 @@
   import Button from "../forms/Button.svelte"
 
   export let modalId: string = "my-modal"
-  export let buttonId: string = "ok-btn"
-  export let buttonTitle: string = "OK"
+  export let modalButtonId: string = "ok-btn"
+  export let modalButtonTitle: string = "OK"
   export let icon: string = "bx-error"
 
   // Au clic sur le bouton, on diffuse _action_
@@ -16,6 +16,54 @@
     })
   }
 </script>
+
+<!-- @component
+  Bouton dont le clic affiche un modal contenant un message d'information et un bouton pour déclencher véritablement l'action
+
+  __Paramètres__ :
+
+  * `modalId` : chaîne représentant l'ID du modal affiché
+  * `modalButtonId` : chaîne représentant l'ID du bouton du modal
+  * `modalButtonTitle`: chaîne reporésentant le titre du bouton
+  * `icon`: nom de l'icone (bibliothèque [Boxicons](https://boxicons.com/?query=))
+
+  __Remarques__ :
+
+  * un slot avec le nom _header_ est disponible pour le titre du message et un autre avec le nom _content_ pour le contenu
+  * le bouton du modal diffuse le mot _action_ lorsqu'il est cliqué afin de déclencher dans le parent avec `on:action`
+  * la fermeture du modal doit être géré dans le parent. Par exemple :
+
+  ```tsx
+  let modal: HTMLElement
+  onMount(async () => {
+    modal = document.getElementById("my-modal")
+  })
+  // click en dehors du modal le fait disparaître
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none"
+    }
+  }
+  // fonction appelée par exemple par `on:action`
+  function handleAction() {
+    modal.style.display = "none"
+  }
+  ```
+
+  __Exemple d'utilisation__ :
+
+  ```tsx
+  <ModalMessageBeforeAction buttonTitle="Continuer" on:action={handleOverLeaf} icon="bxs-error">
+    <span slot="header">Attention !</span>
+    <ul slot="content" class="list-inside list-disc text-left text-base">
+      <li>Il faudra uploader sur Overleaf le package <span class="font-mono bg-coopmaths-warn-100">automultiplechoice.sty</span> pour compiler.</li>
+      <li>Le fichier sortit d’Overleaf ne constitue qu’un aperçu.</li>
+      <li>Le fichier doit être compilé sous AMC impérativement pour que le fichier soit fonctionnel.</li>
+    </ul>
+  </ModalMessageBeforeAction>
+  ```
+
+-->
 
 <div class="fixed hidden inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full" id={modalId}>
   <!-- 
@@ -40,7 +88,7 @@
         </div>
       </div>
       <div class="items-center px-4 py-3">
-        <Button title={buttonTitle} idLabel={buttonId} on:click={triggerAction} />
+        <Button title={modalButtonTitle} idLabel={modalButtonId} on:click={triggerAction} />
       </div>
     </div>
   </div>
