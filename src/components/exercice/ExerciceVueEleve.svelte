e562c861b7d506aac7fd6a295db1157c839497cf
sylcha <chambon.sylvain@gmail.com>
2023-04-12 08:04:19 +0200

    fix: decalage bordure correction (pas convaincant)

--- src/components/exercice/ExerciceVueEleve.svelte
+++ src/components/exercice/ExerciceVueEleve.svelte
@@ -333,7 +333,7 @@
                     </div>
                     <!-- <div class="absolute border-coopmaths-struct dark:border-coopmathsdark-struct top-0 left-0 border-b-[3px] w-10" /> -->
                     <div
-                      class="absolute flex flex-row py-[0.08rem] px-3 rounded-t-md justify-center items-center -left-[0.2rem] -top-[16px] bg-coopmaths-struct dark:bg-coopmathsdark-struct font-semibold text-xs text-coopmaths-canvas dark:text-coopmathsdark-canvas"
+                      class="absolute flex flex-row py-[1.5px] px-3 rounded-t-md justify-center items-center -left-[3px] -top-[15px] bg-coopmaths-struct dark:bg-coopmathsdark-struct font-semibold text-xs text-coopmaths-canvas dark:text-coopmathsdark-canvas"
                     >
                       Correction
                     </div>
