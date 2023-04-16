5721a5c036449adcd4567491863390a6b6f8171a
moragi <86921748+moragi@users.noreply.github.com>
2023-04-16 19:08:20 +0200

    ex: 2N41-8 à tester

--- src/exercices/2e/2N41-8.js
+++ src/exercices/2e/2N41-8.js
@@ -1,6 +1,7 @@
 import Exercice from '../Exercice.js'
-import { listeQuestionsToContenu, randint, choice, combinaisonListes, rienSi1, texFractionReduite, texFraction } from '../../modules/outils.js'
+import { listeQuestionsToContenu, randint, choice, abs, combinaisonListes, rienSi1, reduirePolynomeDegre3, reduireAxPlusB, ecritureAlgebriqueSauf1 } from '../../modules/outils.js'
 import { setReponse } from '../../modules/gestionInteractif.js'
+import FractionX from '../../modules/FractionEtendue.js'
 import { ajouteChampTexteMathLive } from '../../modules/interactif/questionMathLive.js'
 
 export const interactifReady = true
@@ -22,89 +23,220 @@ export default function MettreAuMemeDenominateurLit () {
   this.spacing = 1
   this.spacingCorr = 1
   this.nbQuestions = 2
-  this.sup = 1
-
+  this.sup = 3
+  this.comment = `Les expressions du niveau 1 concerne des expressions du type $a+\\dfrac{b}{x}$ ou $ax+\\dfrac{b}{x}$ ou $a+\\dfrac{b}{cx+d}$.<br>
+  Les expressions de niveau 2 sont plus complexes. Elles nécessitent par exemple un développement du numérateur.`
   this.nouvelleVersion = function () {
     this.sup = parseInt(this.sup)
     this.listeQuestions = [] // Liste de questions
     this.listeCorrections = [] // Liste de questions corrigées
-    const listeFractions = [[1, 2], [1, 3], [2, 3], [1, 4], [3, 4], [1, 5], [2, 5], [3, 5], [4, 5],
-      [1, 6], [5, 6], [1, 7], [2, 7], [3, 7], [4, 7], [5, 7], [6, 7], [1, 8], [3, 8], [5, 8], [7, 8],
-      [1, 9], [2, 9], [4, 9], [5, 9], [7, 9], [8, 9], [1, 10], [3, 10], [7, 10], [9, 10]]
     let typesDeQuestionsDisponibles = []
     if (this.sup === 1) {
-      typesDeQuestionsDisponibles = [1] // coef de x = 1
+      typesDeQuestionsDisponibles = [1, 2, 3]
     } else if (this.sup === 2) {
-      typesDeQuestionsDisponibles = [4, 5, 6] // coef de x > 1
-    } else { typesDeQuestionsDisponibles = [7, 8, 9] } // coef de x relatif
+      typesDeQuestionsDisponibles = [4, 5, 6, 7]
+    } else { typesDeQuestionsDisponibles = [1, 2, 3, 4, 5, 6, 7] } //
 
     const listeTypeDeQuestions = combinaisonListes(typesDeQuestionsDisponibles, this.nbQuestions)
-    for (let i = 0, texte, texteCorr, cpt = 0, a, b, typesDeQuestions, fraction = [], consigne, consigneI, ds, ns; i < this.nbQuestions && cpt < 50;) {
+    for (let i = 0, texte, texteCorr, cpt = 0, typesDeQuestions, consigne1, consigne2, consigneI1, consigneI2; i < this.nbQuestions && cpt < 50;) {
       typesDeQuestions = listeTypeDeQuestions[i]
-      a = randint(1, 9)
-      b = randint(2, 9)
-      fraction = choice(listeFractions)
-      ns = fraction[0]
-      ds = fraction[1]
-      consigne = 'Préciser les valeurs interdites éventuelles, puis écrire l\'expression avec un seul quotient : '
-      consigneI = ' Écrire avec un seul quotient :<br>'
+      consigne1 = 'Préciser les valeurs interdites éventuelles, puis écrire l\'expression avec un seul quotient : '
+      consigne2 = 'Préciser les valeurs interdites éventuelles, puis écrire l\'expression avec un seul quotient (réduire le numérateur) : '
+      consigneI1 = ' Écrire avec un seul quotient :<br>'
+      consigneI2 = ' Écrire avec un seul quotient (réduire le numérateur) :<br>'
       switch (typesDeQuestions) {
-        case 1:
+        case 1:// bx +/- a/x
           { const a = randint(1, 9)
             const b = randint(-9, 9, 0)
-            texte = consigne
-            texte += `$${rienSi1(b)}x+\\dfrac{${a}}{x}$.`
-            texteCorr = `$${rienSi1(b)}x+\\dfrac{${a}}{x}=\\dfrac{${rienSi1(b)}x^2}{x}+\\dfrac{${a}}{x}=\\dfrac{${rienSi1(b)}x^2+${a}}{x}$`
-            const reponse = [`\\dfrac{${b}x^2+${a}}{x}`]
+            const choix = choice([true, false])
+            texte = consigne1
+            texte += `$${rienSi1(b)}x${choix ? '-' : '+'}\\dfrac{${a}}{x}$.`
+            texteCorr = `$0$ est une valeur interdite. <br>
+            Pour $x\\in \\mathbb{R}^*$,<br>
+            $\\begin{aligned}
+            ${rienSi1(b)}x${choix ? '-' : '+'}\\dfrac{${a}}{x}&=\\dfrac{${rienSi1(b)}x^2}{x}${choix ? '-' : '+'}\\dfrac{${a}}{x}\\\\
+           &= \\dfrac{${rienSi1(b)}x^2${choix ? '-' : '+'}${a}}{x}
+           \\end{aligned}$
+        `
+            const reponse = choix ? [`\\dfrac{${b}x^2-${a}}{x}`] : [`\\dfrac{${b}x^2+${a}}{x}`]
             setReponse(this, i, reponse)
             if (this.interactif) {
-              texte = consigneI
-              texte += ` $${rienSi1(b)}x+\\dfrac{${a}}{x}=$` +
+              texte = consigneI1
+              texte += ` $${rienSi1(b)}x${choix ? '-' : '+'}\\dfrac{${a}}{x}=$` +
                     ajouteChampTexteMathLive(this, i, 'largeur25 inline')
             }
           }
           break
-        case 2:
+        case 2:// b +/- a/x
           { const a = randint(1, 9)
             const b = randint(-9, 9, 0)
-            texte = `Préciser les valeurs interdites éventuelles, puis écrire l'expression avec un seul quotient : $${rienSi1(b)}x+\\dfrac{${a}}{x}$.`
-            texteCorr = `$${rienSi1(b)}x+\\dfrac{${a}}{x}=\\dfrac{${rienSi1(b)}x^2}{x}+\\dfrac{${a}}{x}=\\dfrac{${rienSi1(b)}x^2+${a}}{x}$`
-            const reponse = [`\\dfrac{${b}x^2+${a}}{x}`]
+            const choix = choice([true, false])
+            texte = consigne1
+            texte += `$${b}${choix ? '+' : '-'}\\dfrac{${a}}{x}$.`
+            texteCorr = `$0$ est une valeur interdite. <br>
+            Pour $x\\in \\mathbb{R}^*$, <br>
+            $\\begin{aligned}
+            ${b}${choix ? '+' : '-'}\\dfrac{${a}}{x}&=\\dfrac{${rienSi1(b)}x}{x}${choix ? '+' : '-'}\\dfrac{${a}}{x}\\\\
+            &=\\dfrac{${rienSi1(b)}x${choix ? '+' : '-'}${a}}{x}
+            \\end{aligned}$`
+            const reponse = choix ? [`\\dfrac{${b}x+${a}}{x}`] : [`\\dfrac{${b}x-${a}}{x}`]
             setReponse(this, i, reponse)
             if (this.interactif) {
-              texte = ` Écrire avec un seul quotient : $${rienSi1(b)}x+\\dfrac{${a}}{x}=$` +
-                    ajouteChampTexteMathLive(this, i, 'largeur25 inline')
+              texte = consigneI1
+              texte += ` $${b}${choix ? '+' : '-'}\\dfrac{${a}}{x}=$` + ajouteChampTexteMathLive(this, i, 'largeur25 inline')
             }
           }
           break
-        case 3:
-          texte = `$(x-${a})(x+${a})$` // (x-a)(x+a)
-          texteCorr = `$(x-${a})(x+${a})=x^2-${a}^2=x^2-${a * a}$`
+
+        case 3:// a +/- b/(cx+d)
+          { let b
+            const a = randint(-5, 5, 0)
+            const choix = choice([true, false])
+            const c = randint(-2, 5, 0)
+            const k = randint(1, 4)
+            const d = choice([k * c, randint(-5, 5, 0)])
+            b = choice([abs(d - 1), abs(d + 1)])
+            if (b === 0) { b = b + 1 }
+            const f = new FractionX(-d, c).simplifie()
+            texte = consigne2
+            texte += `$${a}${choix ? '+' : '-'}\\dfrac{${b}}{${reduireAxPlusB(c, d)}}$.`
+            texteCorr = `$${f.texFraction}$ est la solution de l'équation $${reduireAxPlusB(c, d)}=0$.<br>
+            $${f.texFraction}$ est donc une valeur interdite pour l'expression. <br>
+            Pour $x\\in \\mathbb{R}\\smallsetminus\\left\\{${f.texFraction}\\right\\}$, <br>
+            $\\begin{aligned}
+${a}${choix ? '+' : '-'}\\dfrac{${b}}{${reduireAxPlusB(c, d)}}&=\\dfrac{${a}(${reduireAxPlusB(c, d)})}{${reduireAxPlusB(c, d)}}${choix ? '+' : '-'}\\dfrac{${b}}{${reduireAxPlusB(c, d)}}\\\\
+            &=\\dfrac{${reduireAxPlusB(a * c, a * d)}${choix ? '+' : '-'}${b}}{${reduireAxPlusB(c, d)}}\\\\
+            &=\\dfrac{${choix ? `${reduireAxPlusB(a * c, a * d + b)}` : `${reduireAxPlusB(a * c, a * d - b)}`}}{${reduireAxPlusB(c, d)}}
+            \\end{aligned}$`
+            const reponse = choix ? [`\\dfrac{${a * c}x+${a * d + b}}{${c}x+${d}}`, `\\dfrac{${-a * c}x+${-a * d - b}}{${-c}x+${-d}}`] : [`\\dfrac{${a * c}x+${a * d - b}}{${c}x+${d}}`, `\\dfrac{${-a * c}x+${-a * d + b}}{${-c}x+${-d}}`]
+            setReponse(this, i, reponse)
+            if (this.interactif) {
+              texte = consigneI1
+              texte += `$${a}${choix ? '+' : '-'}\\dfrac{${b}}{${reduireAxPlusB(c, d)}}=$` + ajouteChampTexteMathLive(this, i, 'largeur25 inline')
+            }
+          }
           break
-        case 4:
-          texte = `$(${b}x+${a})^2$` // (bx+a)²  b>1
-          texteCorr = `$(${b}x+${a})^2=(${b}x)^2+2 \\times ${b}x \\times ${a} + ${a}^2=${b * b}x^2+${2 * b * a}x+${a * a}$`
+
+        case 4:// a/x +/- b/(cx+d)
+          { let b
+            const a = randint(-5, 5, 0)
+            const choix = choice([true, false])
+            const c = randint(-2, 5, 0)
+            const k = randint(1, 4)
+            const d = choice([k * c, randint(-5, 5, 0)])
+            b = choice([abs(d - 1), abs(d + 1)])
+            if (b === 0) { b = b + 1 }
+            const f = new FractionX(-d, c).simplifie()
+            texte = consigne2
+            texte += `$\\dfrac{${a}}{x}${choix ? '+' : '-'}\\dfrac{${b}}{${reduireAxPlusB(c, d)}}$.`
+            texteCorr = `$${f.texFraction}$ est la solution de l'équation $${reduireAxPlusB(c, d)}=0$.<br>
+            $0$ et $${f.texFraction}$ sont donc des valeurs interdites pour l'expression. <br>
+            Pour $x\\in \\mathbb{R}\\smallsetminus\\left\\{${-d / c < 0 ? `${f.texFraction}\\,;\\,0` : `0\\,;\\,${f.texFraction}`}\\right\\}$,<br>
+            $\\begin{aligned} 
+            \\dfrac{${a}}{x}${choix ? '+' : '-'}\\dfrac{${b}}{${reduireAxPlusB(c, d)}}&=\\dfrac{${a}(${reduireAxPlusB(c, d)})}{x(${reduireAxPlusB(c, d)})}${choix ? '+' : '-'}\\dfrac{${rienSi1(b)}x}{x(${reduireAxPlusB(c, d)})}\\\\
+           & =\\dfrac{${reduireAxPlusB(a * c, a * d)}${choix ? '+' : '-'}${rienSi1(b)}x}{x(${reduireAxPlusB(c, d)})}\\\\
+           &=\\dfrac{${choix ? `${reduireAxPlusB(a * c + b, a * d)}` : `${reduireAxPlusB(a * c - b, a * d)}`}}{x(${reduireAxPlusB(c, d)})}
+           \\end{aligned}$`
+            const reponse = choix ? [`\\dfrac{${a * c + b}x+${a * d}}{x(${c}x+${d})}`, `\\dfrac{${-a * c - b}x+${-a * d}}{x(${-c}x+${-d})}`] : [`\\dfrac{${a * c - b}x+${a * d}}{x(${c}x+${d})}`, `\\dfrac{${-a * c + b}x+${-a * d}}{x(${-c}x+${-d})}`]
+            setReponse(this, i, reponse)
+            if (this.interactif) {
+              texte = consigneI2
+              texte += `$\\dfrac{${a}}{x}${choix ? '+' : '-'}\\dfrac{${b}}{${reduireAxPlusB(c, d)}}=$` + ajouteChampTexteMathLive(this, i, 'largeur25 inline')
+            }
+          }
           break
-        case 5:
-          texte = `$(${b}x-${a})^2$` // (bx-a)² b>1
-          texteCorr = `$(${b}x-${a})^2=(${b}x)^2-2 \\times ${b}x \\times ${a} + ${a}^2=${b * b}x^2-${2 * b * a}x+${a * a}$`
+
+        case 5:// ax+b/(cx+d)
+          { let b
+            const a = randint(-3, 9, 0)
+
+            const c = randint(-2, 5, 0)
+            const k = randint(1, 4)
+            const d = choice([k * c, randint(-5, 5, 0)])
+            b = choice([abs(d - 1), abs(d + 1)])
+            if (b === 0) { b = b + 1 }
+            const f = new FractionX(-d, c).simplifie()
+            texte = consigne2
+            texte += `$${rienSi1(a)}x+\\dfrac{${b}}{${reduireAxPlusB(c, d)}}$.`
+            texteCorr = `$${f.texFraction}$ est la solution de l'équation $${reduireAxPlusB(c, d)}=0$.<br>
+            $${f.texFraction}$ est donc une valeur interdite pour l'expression. <br>
+            Pour $x\\in \\mathbb{R}\\smallsetminus\\left\\{${f.texFraction}\\right\\}$, <br>
+            $\\begin{aligned}
+            ${rienSi1(a)}x+\\dfrac{${b}}{${reduireAxPlusB(c, d)}}&=\\dfrac{${a}x(${reduireAxPlusB(c, d)})}{${reduireAxPlusB(c, d)}}+\\dfrac{${b}}{${reduireAxPlusB(c, d)}}\\\\
+            &=\\dfrac{${a * c}x^2${ecritureAlgebriqueSauf1(a * d)}x+${b}}{${reduireAxPlusB(c, d)}}\\\\
+            &=\\dfrac{${reduirePolynomeDegre3(0, a * c, a * d, b)}}{${reduireAxPlusB(c, d)}}
+            \\end{aligned}$`
+            const reponse = [`\\dfrac{${a * c}x^2+${a * d}x+${b}}{${c}x+${d}}`]
+            setReponse(this, i, reponse)
+            if (this.interactif) {
+              texte = consigneI2
+              texte += `$${rienSi1(a)}x+\\dfrac{${b}}{${reduireAxPlusB(c, d)}}=$` + ajouteChampTexteMathLive(this, i, 'largeur25 inline')
+            }
+          }
           break
-        case 6:
-          texte = `$(${b}x-${a})(${b}x+${a})$` // (bx-a)(bx+a) b>1
-          texteCorr = `$(${b}x-${a})(${b}x+${a})=(${b}x)^2-${a}^2=${b * b}x^2-${a * a}$`
+
+        case 6:// ax+e+b/(cx+d)
+          { let b
+            const a = randint(-3, 9, 0)
+            const e = randint(-5, 5, 0)
+            const c = randint(-2, 5, 0)
+            const k = randint(1, 4)
+            const d = choice([k * c, randint(-5, 5, 0)])
+            b = choice([abs(d - 1), abs(d + 1)])
+            if (b === 0) { b = b + 1 }
+            const f = new FractionX(-d, c).simplifie()
+            texte = consigne2
+            texte += `$${reduireAxPlusB(a, e)}+\\dfrac{${b}}{${reduireAxPlusB(c, d)}}$.`
+            texteCorr = `$${f.texFraction}$ est la solution de l'équation $${reduireAxPlusB(c, d)}=0$.<br>
+            $${f.texFraction}$ est donc une valeur interdite pour l'expression. <br>
+            Pour $x\\in \\mathbb{R}\\smallsetminus\\left\\{${f.texFraction}\\right\\}$, <br>
+            $\\begin{aligned}
+            ${reduireAxPlusB(a, e)}+\\dfrac{${b}}{${reduireAxPlusB(c, d)}}
+           & = \\dfrac{(${reduireAxPlusB(a, e)})(${reduireAxPlusB(c, d)})}{${reduireAxPlusB(c, d)}}+\\dfrac{${b}}{${reduireAxPlusB(c, d)}}\\\\
+            &=\\dfrac{(${reduirePolynomeDegre3(0, a * c, a * d + e * c, e * d)})+${b}}{${reduireAxPlusB(c, d)}}\\\\
+           & =\\dfrac{${reduirePolynomeDegre3(0, a * c, a * d + e * c, e * d + b)}}{${reduireAxPlusB(c, d)}}
+           \\end{aligned}$`
+            const reponse = [`\\dfrac{${a * c}x^2+${a * d + e * c}x+${e * d + b}}{${c}x+${d}}`]
+            setReponse(this, i, reponse)
+            if (this.interactif) {
+              texte = consigneI2
+              texte += `$${reduireAxPlusB(a, e)}+\\dfrac{${b}}{${reduireAxPlusB(c, d)}}=$` + ajouteChampTexteMathLive(this, i, 'largeur25 inline')
+            }
+          }
           break
-        case 7:
-          texte = `$\\left(${texFraction(ns, ds)}x+${a}\\right)^2$` // (kx+a)² k rationnel
-          texteCorr = `$\\left(${texFraction(ns, ds)}x+${a}\\right)^2=\\left(${texFraction(ns, ds)}x\\right)^2+2 \\times ${texFraction(ns, ds)}x \\times ${a} + ${a}^2=\\left(${texFraction(ns, ds)}x+${a}\\right)^2=${texFraction(ns * ns, ds * ds)}x^2+${texFractionReduite(ns * 2 * a, ds)}x+${a * a}$`
-          break
-        case 8:
-          texte = `$\\left(${texFraction(ns, ds)}x-${a}\\right)^2$` // (kx-a)² k rationnel
-          texteCorr = `$\\left(${texFraction(ns, ds)}x-${a}\\right)^2=\\left(${texFraction(ns, ds)}x\\right)^2-2 \\times ${texFraction(ns, ds)}x \\times ${a} + ${a}^2=${texFraction(ns * ns, ds * ds)}x^2-${texFractionReduite(ns * 2 * a, ds)}x+${a * a}$`
-          break
-        case 9:
-          //  (bx-a)(bx+a) avec a entier et b rationnel simple
-          texte = `$\\left(${texFraction(ns, ds)}x-${a}\\right)\\left(${texFraction(ns, ds)}x+${a}\\right)$` // b>1
-          texteCorr = `$\\left(${texFraction(ns, ds)}x-${a}\\right)\\left(${texFraction(ns, ds)}x+${a}\\right)=\\left(${texFraction(ns, ds)}x\\right)^2-${a}^2=${texFraction(ns * ns, ds * ds)}x^2-${a * a}$`
+
+        case 7:// a/(ex+f) +/- b/(cx+d)
+          { let b
+            const choix = choice([true, false])
+            const a = randint(-3, 9, 0)
+            const c = randint(-2, 5, 0)
+            const e = randint(1, 9, c)
+            const k = randint(1, 4)
+            const d = choice([k * c, randint(-5, 5, 0)])
+            const f = choice([k * c, randint(-5, 5, 0)])
+            b = choice([abs(d - 1), abs(d + 1)])
+            if (b === 0) { b = b + 1 }
+            const f1 = new FractionX(-d, c).simplifie()
+            const f2 = new FractionX(-f, e).simplifie()
+            texte = consigne2
+            texte += `$\\dfrac{${a}}{${reduireAxPlusB(e, f)}}${choix ? '+' : '-'}\\dfrac{${b}}{${reduireAxPlusB(c, d)}}$.`
+            texteCorr = `$${f1.texFraction}$ est la solution de l'équation $${reduireAxPlusB(c, d)}=0$.<br>
+            $${f2.texFraction}$ est la solution de l'équation $${reduireAxPlusB(e, f)}=0$.<br>
+            $${f1.texFraction}$ et $${f2.texFraction}$ sont donc des valeurs  interdites pour l'expression. <br>
+            Pour $x\\in \\mathbb{R}\\smallsetminus\\left\\{${-d / c < -f / e ? `${f1.texFraction}\\,;\\,${f2.texFraction}` : `${f2.texFraction}\\,;\\,${f1.texFraction}`}\\right\\}$, <br>
+            $\\begin{aligned}
+            \\dfrac{${a}}{${reduireAxPlusB(e, f)}}${choix ? '+' : '-'}\\dfrac{${b}}{${reduireAxPlusB(c, d)}}
+            &= \\dfrac{${a}(${reduireAxPlusB(c, d)})}{(${reduireAxPlusB(e, f)})(${reduireAxPlusB(c, d)})}${choix ? '+' : '-'}\\dfrac{${b}(${reduireAxPlusB(e, f)})}{(${reduireAxPlusB(e, f)})(${reduireAxPlusB(c, d)})}\\\\
+            =\\dfrac{${a}(${reduireAxPlusB(c, d)})${choix ? `${ecritureAlgebriqueSauf1(b)}` : `${ecritureAlgebriqueSauf1(-b)}`}(${reduireAxPlusB(e, f)})}{(${reduireAxPlusB(e, f)})(${reduireAxPlusB(c, d)})}\\\\
+            =\\dfrac{${choix ? `${reduireAxPlusB(a * c + b * e, a * d + b * f)}` : `${reduireAxPlusB(a * c - b * e, a * d - b * f)}`}}{(${reduireAxPlusB(e, f)})(${reduireAxPlusB(c, d)})}
+            \\end{aligned}$`
+            const reponse = choix ? [`\\dfrac{${a * c + b * e}x+${a * d + b * f}}{(${e}x+${f})(${c}x+${d})}`, `\\dfrac{${-a * c - b * e}x+${-a * d - b * f}}{(${-c}x+${-d})(${e}x+${f})}`, `\\dfrac{${-a * c - b * e}x+${-a * d - b * f}}{(${c}x+${d})(${-e}x+${-f})}`] : [`\\dfrac{${a * c - b * e}x+${a * d - b * f}}{(${e}x+${f})(${c}x+${d})}`, `\\dfrac{${-a * c + b * e}x+${-a * d + b * f}}{(${-e}x+${-f})(${c}x+${d})}`, `\\dfrac{${-a * c + b * e}x+${-a * d + b * f}}{(${e}x+${f})(${-c}x+${-d})}`]
+            setReponse(this, i, reponse)
+            if (this.interactif) {
+              texte = consigneI2
+              texte += `$\\dfrac{${a}}{${reduireAxPlusB(e, f)}}${choix ? '+' : '-'}\\dfrac{${b}}{${reduireAxPlusB(c, d)}}=$` + ajouteChampTexteMathLive(this, i, 'largeur25 inline')
+            }
+          }
           break
       }
 
@@ -118,5 +250,5 @@ export default function MettreAuMemeDenominateurLit () {
     }
     listeQuestionsToContenu(this)
   }
-  this.besoinFormulaireNumerique = ['Niveau de difficulté', 3, '1 : Coefficient de x égal à 1\n 2 : Coefficient de x supérieur à 1\n 3 : Coefficient de x rationnel']
+  this.besoinFormulaireNumerique = ['Niveau de difficulté', 3, '1 : Niveau 1\n 2 : Niveau 2\n 3 : Mélange']
 }
