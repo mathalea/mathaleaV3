26ed66a39e291d25b5059bd5dc9518571dd952fb
moragi <86921748+moragi@users.noreply.github.com>
2023-04-20 16:55:58 +0200

    ex: 2N52-5 coquilles Christophe/Maxime/Stéphane

--- src/exercices/2e/2N52-5.js
+++ src/exercices/2e/2N52-5.js
@@ -86,9 +86,9 @@ x&= ${f2.texFraction}
 \\end{aligned}$<br>`
           }
 
-          texteCorr += ` $${f2.texFraction}$ n'est pas une valeur interdite, donc l' ensemble des solutions de cette équation est  $\\mathscr{S}=\\left\\{${f2.texFraction}\\right\\}$.`
+          texteCorr += ` $${f2.texFraction}$ n'est pas une valeur interdite, donc l'ensemble des solutions de cette équation est  $\\mathscr{S}=\\left\\{${f2.texFraction}\\right\\}$.`
           break
-        case 2:// (ax^2-b)/(cx+d)=0
+        case 2:// (ax^2+/-b)/(cx+d)=0
           a = randint(1, 4)
           k1 = randint(1, 10)
           b = a * k1 * k1
@@ -98,47 +98,71 @@ x&= ${f2.texFraction}
 
           choix = choice([true, false])
           texte = consigne1
-          texte += `${choix ? `$\\dfrac{${rienSi1(a)}x^2-${b}}{${reduireAxPlusB(c, d)}}=0$` : `$\\dfrac{${b}-${rienSi1(a)}x^2}{${reduireAxPlusB(c, d)}}=0$`}.`
-          if (context.isDiaporama) {
-            texteCorr = ''
-          } else {
-            texteCorr = 'Déterminer les valeurs interdites revient à déterminer les valeurs qui annulent le dénominateur du quotient, puisque la division par $0$ n\'existe pas.<br>'
-          }
-          texteCorr += `Or $${reduireAxPlusB(c, d)}=0$ si et seulement si  $x=${-k2}$. <br>
+          if (choice([true, false])) {
+            texte += `${choix ? `$\\dfrac{${rienSi1(a)}x^2-${b}}{${reduireAxPlusB(c, d)}}=0$` : `$\\dfrac{${b}-${rienSi1(a)}x^2}{${reduireAxPlusB(c, d)}}=0$`}.`
+            if (context.isDiaporama) {
+              texteCorr = ''
+            } else {
+              texteCorr = 'Déterminer les valeurs interdites revient à déterminer les valeurs qui annulent le dénominateur du quotient, puisque la division par $0$ n\'existe pas.<br>'
+            }
+            texteCorr += `Or $${reduireAxPlusB(c, d)}=0$ si et seulement si  $x=${-k2}$. <br>
           Donc l'ensemble des valeurs interdites est  $\\left\\{${-k2}\\right\\}$.<br>
           Pour tout $x\\in \\mathbb{R}\\smallsetminus\\left\\{${-k2}\\right\\}$, <br>
             $\\begin{aligned}
             ${choix ? `\\dfrac{${rienSi1(a)}x^2-${b}}{${reduireAxPlusB(c, d)}}&=0` : `\\dfrac{${b}-${rienSi1(a)}x^2}{${reduireAxPlusB(c, d)}}&=0`}\\\\
             ${choix ? `${rienSi1(a)}x^2-${b}&=0` : `${b}-${rienSi1(a)}x^2&=0`}${sp(7)} \\text{ car }\\dfrac{A(x)}{B(x)}=0 \\text { si et seulement si } A(x)=0 \\text { et } B(x)\\neq 0\\\\
+            ${rienSi1(a)}x^2&=${b}\\\\
             x^2&=${k1 * k1}\\\\
            x= ${k1}&\\text{ ou } x= -${k1}
            \\end{aligned}$<br>
            `
-          if (-k2 === k1 || k2 === k1) {
-            if (-k2 === k1) {
-              texteCorr += `  $${k1}$ est une valeur interdite, donc l' ensemble des solutions de cette équation est  $\\mathscr{S}=\\left\\{${-k1}\\right\\}$.
+            if (-k2 === k1 || k2 === k1) {
+              if (-k2 === k1) {
+                texteCorr += `  $${k1}$ est une valeur interdite, donc l'ensemble des solutions de cette équation est  $\\mathscr{S}=\\left\\{${-k1}\\right\\}$.
         `
+              } else {
+                texteCorr += `  $${-k1}$ est une valeur interdite, donc l'ensemble des solutions de cette équation est  $\\mathscr{S}=\\left\\{${k1}\\right\\}$.
+        `
+              }
             } else {
-              texteCorr += `  $${-k1}$ est une valeur interdite, donc l' ensemble des solutions de cette équation est  $\\mathscr{S}=\\left\\{${k1}\\right\\}$.
+              texteCorr += `  $${-k1}$ et $${k1}$ ne sont pas des valeurs interdites, donc l'ensemble des solutions de cette équation est  $\\mathscr{S}=\\left\\{${-k1}\\,;\\,${k1}\\right\\}$.
         `
             }
           } else {
-            texteCorr += `  $${-k1}$ et $${k1}$ ne sont pas des valeurs interdites, donc l' ensemble des solutions de cette équation est  $\\mathscr{S}=\\left\\{${-k1}\\,;\\,${k1}\\right\\}$.
-        `
+            texte += `${choix ? `$\\dfrac{${rienSi1(a)}x^2+${b}}{${reduireAxPlusB(c, d)}}=0$` : `$\\dfrac{${b}+${rienSi1(a)}x^2}{${reduireAxPlusB(c, d)}}=0$`}.`
+            if (context.isDiaporama) {
+              texteCorr = ''
+            } else {
+              texteCorr = 'Déterminer les valeurs interdites revient à déterminer les valeurs qui annulent le dénominateur du quotient, puisque la division par $0$ n\'existe pas.<br>'
+            }
+            texteCorr += `Or $${reduireAxPlusB(c, d)}=0$ si et seulement si  $x=${-k2}$. <br>
+Donc l'ensemble des valeurs interdites est  $\\left\\{${-k2}\\right\\}$.<br>
+Pour tout $x\\in \\mathbb{R}\\smallsetminus\\left\\{${-k2}\\right\\}$, <br>
+  $\\begin{aligned}
+  ${choix ? `\\dfrac{${rienSi1(a)}x^2+${b}}{${reduireAxPlusB(c, d)}}&=0` : `\\dfrac{${b}+${rienSi1(a)}x^2}{${reduireAxPlusB(c, d)}}&=0`}\\\\
+  ${choix ? `${rienSi1(a)}x^2+${b}&=0` : `${b}+${rienSi1(a)}x^2&=0`}${sp(7)} \\text{ car }\\dfrac{A(x)}{B(x)}=0 \\text { si et seulement si } A(x)=0 \\text { et } B(x)\\neq 0\\\\
+  ${rienSi1(a)}x^2&=-${b}\\\\
+  x^2&=-${k1 * k1}
+ \\end{aligned}$<br>
+ `
+            texteCorr += `Puisque $-${k1 * k1}<0$, cette équation n'a pas de solution, donc l'ensemble des solutions est  $\\mathscr{S}=\\varnothing$.
+`
           }
+
           break
         case 3:// (ax+b)/(cx+d)=e
-          a = randint(-3, 9, 0)
+          a = randint(-3, 5, 0)
           b = randint(-9, 9)
           c = randint(-9, 9, 0)
           d = randint(-9, 9)
           e = randint(-9, 9, 0)
-          if (a * d - b * c === 0) {
-            a = randint(-3, 9, 0)
-            b = randint(-9, 9, 0)
+          while ((a * d - b * c === 0) || (a - e * c === 0)) {
+            a = randint(-3, 5)
+            b = randint(-9, 9)
             c = randint(-9, 9, 0)
             d = randint(-9, 9)
-          }
+            e = randint(-9, 9, 0)
+          };
           f1 = new FractionX(-d, c).simplifie()
           f2 = new FractionX(e * d - b, a - e * c).simplifie()
           choix = choice([true, false])
@@ -151,7 +175,7 @@ x&= ${f2.texFraction}
             texteCorr = 'Déterminer les valeurs interdites revient à déterminer les valeurs qui annulent le dénominateur du quotient, puisque la division par $0$ n\'existe pas.<br>'
           }
           texteCorr += `Or $${reduireAxPlusB(c, d)}=0$ si et seulement si  $x=${f1.texFraction}$. <br>
-          Donc l'ensemble des valeurs interdites est  $\\left\\{${f1.texFraction}\\right\\}$ est une valeur interdite pour l'équation. <br>
+          Donc l'ensemble des valeurs interdites est  $\\left\\{${f1.texFraction}\\right\\}$. <br>
           Pour tout $x\\in \\mathbb{R}\\smallsetminus\\left\\{${f1.texFraction}\\right\\}$,<br>`
           if (b === 0) {
             texteCorr += ` 
@@ -174,8 +198,8 @@ x&= ${f2.texFraction}
           }
 
           if (-d * (a - e * c) - c * (e * d - b) === 0) {
-            texteCorr += `$${f2.texFraction}$ est  une valeur interdite, donc l' ensemble des solutions de cette équation est  $\\mathscr{S}=\\varnothing$.`
-          } else { texteCorr += `$${f2.texFraction}$ n'est pas une valeur interdite, donc l' ensemble des solutions de cette équation est  $\\mathscr{S}=\\left\\{${f2.texFraction}\\right\\}$.` }
+            texteCorr += `$${f2.texFraction}$ est  une valeur interdite, donc l'ensemble des solutions de cette équation est  $\\mathscr{S}=\\varnothing$.`
+          } else { texteCorr += `$${f2.texFraction}$ n'est pas une valeur interdite, donc l'ensemble des solutions de cette équation est  $\\mathscr{S}=\\left\\{${f2.texFraction}\\right\\}$.` }
           break
 
         case 4:// e/(ax+b)=f/(cx+d)
@@ -185,7 +209,7 @@ x&= ${f2.texFraction}
           d = randint(-9, 9)
           e = randint(-9, 9, 0)
           f = randint(-9, 9, 0)
-          if (c * (f * b - a * d) === -d * (e * c - f * a) || a * (f * b - a * d) === -b * (e * c - f * a)) { // pas de VI sol enfin normalement :-)
+          while ((c * (f * b - a * d) === -d * (e * c - f * a)) || (a * (f * b - a * d) === -b * (e * c - f * a))) { // pas de VI sol enfin normalement :-)
             a = randint(-3, 9, 0)
             b = randint(-9, 9)
             c = randint(-5, 9, 0)
@@ -206,14 +230,14 @@ x&= ${f2.texFraction}
             texteCorr = 'Déterminer les valeurs interdites revient à déterminer les valeurs qui annulent les dénominateurs des quotients, puisque la division par $0$ n\'existe pas.<br>'
           }
           texteCorr += `Or $${reduireAxPlusB(a, b)}=0$ si et seulement si  $x=${f2.texFraction}$ et $${reduireAxPlusB(c, d)}=0$ si et seulement si  $x=${f1.texFraction}$. <br>
-          Donc l'ensemble des valeurs interdites est  $\\left\\{${-d / c < -b / a ? `${f1.texFraction}\\,;\\,${f2.texFraction}` : `${f2.texFraction}\\,;\\,${f1.texFraction}`}\\right\\}$ sont des  valeurs interdites pour l'équation. <br>`
+          Donc l'ensemble des valeurs interdites est  $\\left\\{${-d / c < -b / a ? `${f1.texFraction}\\,;\\,${f2.texFraction}` : `${f2.texFraction}\\,;\\,${f1.texFraction}`}\\right\\}$. <br>`
 
           texteCorr += `Pour tout $x\\in \\mathbb{R}\\smallsetminus\\left\\{${-d / c < -b / a ? `${f1.texFraction}\\,;\\,${f2.texFraction}` : `${f2.texFraction}\\,;\\,${f1.texFraction}`}\\right\\}$,<br>
  $\\begin{aligned}
  \\dfrac{${e}}{${reduireAxPlusB(a, b)}}&=\\dfrac{${f}}{${reduireAxPlusB(c, d)}}\\\\
  ${f}\\times (${reduireAxPlusB(a, b)})&=${e}\\times (${reduireAxPlusB(c, d)})${sp(7)} \\text{ car les produits en croix sont égaux.}\\\\
  ${reduireAxPlusB(f * a, f * b)}&=${reduireAxPlusB(e * c, e * d)}\\\\
-${e * c - f * a}x&= ${-e * d + f * b}\\\\
+${-e * c + f * a}x&= ${e * d - f * b}\\\\
 x&=${f3.texFraction}
 \\end{aligned}$<br>`
 
